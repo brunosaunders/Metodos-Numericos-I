89388d4dafd1177497115c8574b0f3f6ea6389b0
Bruno Sena Saunders <brunossaunders@pop-os.localdomain>
2023-10-14 16:52:32 -0300

    cmakelists e headers modificados

--- include/Pendulo.hpp
+++ include/Pendulo.hpp
@@ -1,12 +1,13 @@
 #ifndef PENDULO_HPP
 #define PENDULO_HPP
+
 #include "Polinomio.hpp"
 /**
  * Representação do polinômio que modela a oscilação de um pêndulo: 
  * f(d) = a_3d^3 – 9a_2d + 3 
 */
 class Pendulo:public Polinomio { 
-public:
-    Pendulo (double a3,double a2) : Polinomio({a3,0,9*a2,3}){}
+    public:
+        Pendulo (double a3,double a2) : Polinomio({a3,0,9*a2,3}){}
 }; 
 #endif
