250ff0fb4193daba2cd82a0c86a1825c58d59922
ismaelgomesdev <ismaelgomes@alu.ufc.br>
2023-10-18 19:21:02 -0300

    Implementação inicial do método de Newton original

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <random>
+#include <vector> 
+#include <iterator> 
+
 #include "Polinomio.hpp"
 #include "NewtonRaphsonComDerivadaNumerica.hpp"
 #include "NewtonRaphsonFL.hpp"
 #include "Passos.hpp"
-#include <vector> 
-#include <iterator> 
 #include "NewtonRaphson.hpp"
 #include "Pendulo.hpp"
-#include <iomanip>
-#include <cmath>
-#include <random>
+
 
 void print_vector(std::vector<double> v) {
     std::cout << "[";
@@ -49,20 +51,20 @@ void testa_passos() {
 
 
 int main() {
-    Polinomio p = Polinomio({ 1.0, 0, -9.0, 3.0 }); // x³ - 9x + 3 ----> Derivada = 3x^2 - 9
-    std::cout << p.get_grau() << std::endl;
-    std::cout << p.get_valor_funcao(10.0) << std::endl;
+    // Polinomio p = Polinomio({ 1.0, 0, -9.0, 3.0 }); // x³ - 9x + 3 ----> Derivada = 3x^2 - 9
+    // std::cout << p.get_grau() << std::endl;
+    // std::cout << p.get_valor_funcao(10.0) << std::endl;
 
-    print_vector(p.get_funcao_derivada().coeficientes);
+    // print_vector(p.get_funcao_derivada().coeficientes);
 
-    NewtonRaphsonComDerivadaNumerica numerica(1000, 0.001, p);
-    NewtonRaphsonFL fl(1000, 0.001, p, 0.05);
+    // NewtonRaphsonComDerivadaNumerica numerica(1000, 0.001, p);
+    // NewtonRaphsonFL fl(1000, 0.001, p, 0.05);
 
-    numerica.calcula_raiz(0.5);
-    fl.calcula_raiz(0.5);
+    // numerica.calcula_raiz(0.5);
+    // fl.calcula_raiz(0.5);
 
-    print_vector(numerica.get_iteracoes_de_x());
-    print_vector(fl.get_iteracoes_de_x());
+    // print_vector(numerica.get_iteracoes_de_x());
+    // print_vector(fl.get_iteracoes_de_x());
 
     //Testa passos
     testa_passos();
--- src/metodos/NewtonRaphson.cpp
+++ src/metodos/NewtonRaphson.cpp
@@ -70,4 +70,41 @@ double NewtonRaphson::get_raiz() {
 
 void NewtonRaphson::calcula_raiz(double x0) {
     /* TODO: Ismael */
+
+    int k = 1;
+    double x_k = 0.0, raiz = 0.0;
+    bool continuar_iteracao = true;
+
+    // Checando se o x inicial já é próximo o suficiente da raiz.
+    if (std::abs(this->funcao.get_valor_funcao(x0)) < this->erro) {
+        raiz = x0;
+        continuar_iteracao = false;
+    }
+
+    this->iteracoes_de_x.push_back(x0);
+
+    while (continuar_iteracao) {
+        double f_x = this->funcao.get_valor_funcao(x0);
+        double f_prime_x = this->funcao.get_funcao_derivada().get_valor_funcao(x0);
+
+        x_k = x0 - (f_x / f_prime_x);
+        this->iteracoes_de_x.push_back(x_k);
+
+        /* Resultado do método é invalidado por estouro do número máximo de iterações */
+        if (k > this->max_iteracoes) {
+            this->raiz_valida = false;
+            this->num_passos = max_iteracoes;
+            return;
+        }
+
+        if ((std::abs(f_x) < this->erro) && (std::abs(x_k - x0) < this->erro)) {
+            raiz = x_k;
+            continuar_iteracao = false;
+        }
+
+        x0 = x_k;
+        k++;
+    }
+
+    this->num_passos = this->iteracoes_de_x.size();
 }
