d85376eb0961fed58aad0579a8d6ea0400646bd4
ismaelgomesdev <ismaelgomes@alu.ufc.br>
2023-10-18 19:21:02 -0300

    Implementação inicial do método de Newton original

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Polinomio.hpp"
+#include "NewtonRaphson.hpp"
 
 void print_vector(std::vector<double> v) {
     std::cout << "[";
@@ -10,9 +11,10 @@ void print_vector(std::vector<double> v) {
 }
 
 int main() {
-    Polinomio p = Polinomio({1.0, 0, -3.0, 2.0}); // x³ - 3x + 2
-    std::cout << p.get_grau() << std::endl;
-    std::cout << p.get_valor_funcao(10.0) << std::endl;
-    
-    print_vector(p.get_funcao_derivada().coeficientes);
+    std::vector<double> coef = {-2, 1, 1}; // Cria um vetor de coeficientes para o polinômio com a3 = 1, a2 = 1 e a1 = -2
+    Polinomio p(coef); // Cria um polinômio usando o vetor de coeficientes
+    NewtonRaphson nr(10, 0.001, p); // Cria um objeto da classe NewtonRaphson com os parâmetros dados com max_iteracoes = 10, erro = 0.001 e funcao = p
+    nr.calcula_raiz(0.5); // Calcula a raiz da função usando o método de Newton-Raphson com x0 = 0.5
+    std::cout << "A raiz encontrada foi: " << nr.get_raiz() << std::endl; // Imprime a raiz encontrada
+    return 0;
 }
