45c6e4eca484c2aae5980fbb3ff542460bdf5576
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2023-10-21 18:33:16 -0300

    Update painel.cpp
    Pessoal, atualizei aqui o painel, ainda falta algumas coisas porque eu não tenho a classe NewtonRaphson comigo, então eu criei uma classe provisória dentro do do painel.cpp, mas no final eu vou tirar ela e eu acho que não vai dar muito trabalho adaptar com o NewtonRaphson final. Coloquei também os placeholders das funções de mostrar o quadro comparativo e o método de isolamento.
    Quando puderem, deem uma olhada para ver se eu não esqueci nada, por favor

--- src/painel.cpp
+++ src/painel.cpp
@@ -1,47 +1,158 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    double prec;
-    int n, i;
+class NewtonRaphson 
+{
+ public:
+     double a3, a2, prec, lambda;
+     string met;
 
-    cout << "Vamos calcular o deslocamento da oscilaçao de um determinado balanco.\n";
-    cout << "Primeiramente, insira o numero de lambdas que serao testados:\n";
-    cin >> n;
-    double* lambda = new double[n];
-    double* coef3 = new double[n];
-    double* coef2 = new double[n];
-    cout << "Em seguida, insira o(s) lambda(s):\n";
-    for (i = 0; i < n; i++)
+     NewtonRaphson() : a3(0.0), a2(0.0), prec(0.0), lambda(0.0), met(""){}
+};
+
+
+void painel(vector<NewtonRaphson> funcoes)
+{
+    int i;
+
+    cout << "Escolha uma opcao:\n1 -> Cadastrar uma funcao e metodo para analise.\n2 -> Remover uma funcao ja cadastrada.\n";
+    cout << "3 -> Resetar funcoes cadastradas.\n4 -> Mostrar analise passo a passo de um funcao.\n5 -> Gerar quadro comparativo das funcoes cadastradas.\n";
+    cout << "6 -> Mostrar isolamento de uma funcao.\n0 -> Sair.\n";
+    cin >> i;
+    if (i == 0)
     {
-        cin >> lambda[i] >> coef2[i] >> coef3[i];
+        return;
     }
-    cout << "Por fim, insira a precisao que deseja adotar para o calculo:\n";
-    cin >> prec;
-    cout << "Agora, comecaremos calculando o deslocamento pelo Metodo de Newton original:\n";
-    //a) função metodo de newton [usar o exemplo do item d) nos itens a), b) e c)?]
-    cout << "Depois, calculamos o deslocamento pelo Metodo de Newton com FL:\n";
-    //b) função metodo de newton FL (explicar o que seria o FL?)
-    cout << "Continuando, calculamos o deslocamento utilizando a derivada de f(d):\n";
-    //c) função com derivada
-    cout << "Com isso, obtemos os seguintes quadros respostas para cada metodo:\n";
-    //e) mostrar tabelas resposta
-    cout << "Comparando os resultados em um quadro comparativo...\n";
-    //f) mostrar tabela comparativa
-    cout << "Nos podemos chegar na seguinte analise:\n";
+    else if (i == 1)
+    {
+        NewtonRaphson nr;
+        int j;
 
+        cout << "Voce escolheu 1 -> Cadastrar uma funcao e metodo para analise.\n\n";
+        cout << "Insira os seguintes dados: a3, a2 e precisao\n";
+        cin >> nr.a3 >> nr.a2 >> nr.prec;
+        cout << "Agora, escolha o metodo desejado:\n1 -> Newton Raphson (NR).\n2 -> Newton Raphson com FL (NR-FL).\n";
+        cout << "3 -> Newton Raphson com Derivada Numerica (NR-D).\n";
+        cin >> j;
+        if (j == 1)
+        {
+            cout << "Voce escolheu 1 -> Newton Raphson (NR).\n\n";
+            nr.met = "NR";
+            funcoes.push_back(nr);
+            cout << "Metodo cadastrado: NR(a3: " << nr.a3 << ", a2: " << nr.a2 << ", e: " << nr.prec << ")\n\n";
+        }
+        if (j == 2)
+        {
+            cout << "Voce escolheu 2 -> Newton Raphson com FL (NR-FL).\n";
+            nr.met = "NR-Fl";
+            cout << "Informe o lambda:\n";
+            cin >> nr.lambda;
+            funcoes.push_back(nr);
+            cout << "Metodo cadastrado: NR-FL(a3: " << nr.a3 << ", a2: " << nr.a2 << ", e: " << nr.prec << ", lambda: " << nr.lambda << ")\n\n";
+        }
+        if (j == 3)
+        {
+            cout << "Voce escolheu 3 -> Newton Raphson com Derivada Numerica (NR-D).\n\n";
+            nr.met = "NR-D";
+            funcoes.push_back(nr);
+            cout << "Metodo cadastrado: NR-D(a3: " << nr.a3 << ", a2: " << nr.a2 << ", e: " << nr.prec << ")\n\n";
+        }
+        painel(funcoes);
+    }
+    else if (i == 2)
+    {
+        cout << "Voce escolheu 2 -> Remover uma funcao ja cadastrada.\n\n";
+        if (funcoes.size() < 1)
+        {
+            cout << "Ainda nao foram cadastradas funcoes. Portanto, nao e possivel remover nenhuma. ";
+            cout << "Tente inserir uma funcao primeiramente.\n\n";
+        }
+        else
+        {
+            int j;
+            cout << "Estas sao as funcoes cadastradas:\n\n";
+            for (int i = 0; i < funcoes.size(); i++) 
+            {
+                cout << i + 1 << " -> " << funcoes[i].met << "(" << funcoes[i].a3 << " " << funcoes[i].a2 << " " << funcoes[i].prec << " " << funcoes[i].lambda << ")\n";
+            }
+            cout << "\nInsira o indice de qual funcao deseja remover:\n";
+            cin >> j;
+            funcoes.erase(funcoes.begin() + (j - 1));
+            cout << "Funcao removida com sucesso.\n\n";
+        }
+        painel(funcoes);
+    }
+    else if (i == 3)
+    {
+        cout << "Voce escolheu 3 -> Resetar funcoes cadastradas.\n\n";
+        if (funcoes.size() < 1)
+        {
+            cout << "Nao existem funcoes cadastradas. Portanto, nao ha o que remover.\n\n";
+        }
+        else
+        {
+            funcoes.clear();
+            cout << "Pronto, todas as funcoes foram removidas.\n\n";
+        }
+        painel(funcoes);
+    }
+    else if (i == 4)
+    {
+        cout << "Voce escolheu 4 -> Mostrar analise passo a passo de um funcao.\n\n";
+        if (funcoes.size() < 1)
+        {
+            cout << "Ainda nao foram cadastradas funcoes. Portanto, nao e possivel analisar nenhuma. ";
+            cout << "Tente inserir uma funcao primeiramente.\n\n";
+        }
+        else
+        {
+            int j;
+            cout << "Estas sao as funcoes cadastradas:\n\n";
+            for (int i = 0; i < funcoes.size(); i++)
+            {
+                cout << i + 1 << " -> " << funcoes[i].met << "(" << funcoes[i].a3 << " " << funcoes[i].a2 << " " << funcoes[i].prec << " " << funcoes[i].lambda << ")\n";
+            }
+            cout << "\nInsira o indice de qual funcao deseja analisar:\n";
+            cin >> j;
+            if (funcoes[j - 1].met == "NR")
+            {
+                //chamar metodo NR
+            }
+            else if (funcoes[j - 1].met == "NR-FL")
+            {
+                //chamar metodo NR-FL
+            }
+            else if (funcoes[j - 1].met == "NR-D")
+            {
+                //chamar metodo NR-D
+            }
+        }
+        painel(funcoes);
+    }
+    else if (i == 5)
+    {
+        cout << "Voce escolheu 5 -> Gerar quadro comparativo das funcoes cadastradas.\n\n";
+        //chamar metodo que mostra quadro comparativo
+        painel(funcoes);
+    }
+    else if (i == 6)
+    {
+        cout << "Voce escolheu 6 -> Mostrar isolamento de uma funcao.\n\n";
+        //chamar metodo que mostra o isolamento da funcao
+        painel(funcoes);
+    }
+    else
+    {
+        cout << "Entrada invalida. Tente novamente.\n";
+        painel(funcoes);
+    }
+}
 
-
-    //cout << "n = " << n << "\n";
-    //for (i = 0; i < n; i++)
-    //{
-       // cout << "lambda " << i+1 << "=" << lambda[i] << "\n";
-    //}
-    //cout << "prec = " << prec << "\n";
-
-    delete[] lambda;
-    delete[] coef3;
-    delete[] coef2;
+int main() 
+{
+    vector<NewtonRaphson> funcoes;
+    painel(funcoes);
     return 0;
 }
