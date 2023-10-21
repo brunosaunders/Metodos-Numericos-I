5cccc42709bcbd2d6810086fceb39b32803ae706
DiegoCaracasMendes <diegocaracasmendes02@gmail.com>
2023-10-21 00:47:45 -0300

    Implementação NewtonFL

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include "Polinomio.hpp"
+#include "NewtonRaphsonComDerivadaNumerica.hpp"
+#include "NewtonRaphsonFL.hpp"
 
 void print_vector(std::vector<double> v) {
     std::cout << "[";
-    for (auto &item : v) {
+    for (auto& item : v) {
         std::cout << item << " ";
     }
     std::cout << "]\n";
 }
 
 int main() {
-    Polinomio p = Polinomio({1.0, 0, -3.0, 2.0}); // x³ - 3x + 2
+    Polinomio p = Polinomio({ 1.0, 0, -9.0, 3.0 }); // x³ - 9x + 3 ----> Derivada = 3x^2 - 9
     std::cout << p.get_grau() << std::endl;
     std::cout << p.get_valor_funcao(10.0) << std::endl;
-    
+
     print_vector(p.get_funcao_derivada().coeficientes);
+
+    NewtonRaphsonComDerivadaNumerica numerica(1000, 0.001, p);
+    NewtonRaphsonFL fl(1000, 0.001, p, 0.05);
+
+    numerica.calcula_raiz(0.5);
+    fl.calcula_raiz(0.5);
+
+    print_vector(numerica.get_iteracoes_de_x());
+    print_vector(fl.get_iteracoes_de_x());
+
+    return 0;
 }
