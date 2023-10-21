22f13d204cd906494e6639c76fed27340cc4bf38
israel <israelnicolas940@gmail.com>
2023-10-21 17:15:45 -0300

    outputs padronizados com teste aleatório
    Resolução de conflitos (22/10/2023)

--- include/Vizualizador.hpp
+++ include/Vizualizador.hpp
@@ -5,10 +5,29 @@
 #include "Polinomio.hpp"
 #include <vector>
 
-class Vizualizador { 
-public: 
-        Vizualizador (); 
-        void print_quadro_coeficientes_fixos(Polinomio,std::vector<Metodo*>,int);
-}; 
+namespace vizualizador
+{
+    class EntradasLinhaQuadroComparativo { 
+    public: 
+        int metodo; // 0-> original, 1->FL, 2 -> Derivada
+        double a3; 
+        double a2; 
+        double  lambda; 
+        double epsilon; 
+        inline std::string get_nome_metodo(); 
+    }; 
+    class SaidasLinhaQuadroComparativo { 
+    public: 
+        EntradasLinhaQuadroComparativo* entrada;
+        double x_k;
+        double e1; // erro obtido por |x_{k} - x_{k-1}|
+        double e2; // erro obtido por |f(x_{k})|
+        int iteracoes;  
+    };
+    void calcula_tamanho_blocos(int *); 
+    void print_header(int *tamanho_blocos);
+    void print_quadro_comparativo(std::vector<EntradasLinhaQuadroComparativo> linhas,int) ; 
+    void print_linha_quadro_comparativo(SaidasLinhaQuadroComparativo *linha, int *tamanho_blocos,int); 
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <vector> 
 #include <iterator> 
 #include "Pendulo.hpp"
+#include <iomanip>
+#include <cmath>
+#include <random>
 
 void print_vector(std::vector<double> v, int precisao) {
     std::cout << std::fixed << std::setprecision(precisao);
@@ -18,19 +21,36 @@ void print_vector(std::vector<double> v, int precisao) {
     std::cout << "]\n";
 }
 
-void teste_vizualizador(){ 
-    Vizualizador v; 
-    std::vector<Polinomio> polinomios{Polinomio({1.0,-3.0,2.0}),Pendulo(10,1), Polinomio({77.0,29.0,38.0,21.0,57.0,66.99})} ; 
+void teste_vizualizador() {
+    std::vector<vizualizador::EntradasLinhaQuadroComparativo> entradas;
 
-    // Teste de vizualização 
-    NewtonRaphson nr(23,0.9);  
-    nr.original(polinomios[1]);
-    NewtonRaphson nr2(10,0.1);  
-    nr2.original(polinomios[1]);
-    v.print_quadro_coeficientes_fixos(polinomios[1],{&nr,&nr2},8); 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> double_dist(1.0, 1000.0);
 
+    for (int i = 0; i < 100; i++) {
+        int metodo = i % 3;  // Alternar entre os três métodos (0, 1, 2)
+        double a3 = double_dist(gen);
+        double a2 = double_dist(gen);
+        double lambda = double_dist(gen);
+        double epsilon = double_dist(gen);
+
+        entradas.push_back(vizualizador::EntradasLinhaQuadroComparativo{metodo, a3, a2, lambda,epsilon});
+    }
+    // std::vector<vizualizador::EntradasLinhaQuadroComparativo> entradas {
+    //     {2, 4123.4124, 1245.2133002, 123.2, 999.3939},
+    //     {0, 10.9, 22.0, 72.2},
+    //     {1, 10.9, 22.0, 72.2},
+    //     {0, 5.6, 11.0, 41.3},
+    //     {1, 7.8, 16.5, 63.7},
+    //     {2, 15.2, 31.4, 121.9},
+    //     {0, 3.2, 7.0, 31.6},
+    //     {1, 12.5, 28.8, 112.4},
+    //     {2, 21.7, 42.3, 167.8},
+    //     {0, 6.1, 13.7, 55.9}
+    // };
+    vizualizador::print_quadro_comparativo(entradas, 2);
 }
-
 int main() {
     int precisao = 6;
     std::vector<double> coef = {-2, 1, 1}; 
@@ -42,11 +62,13 @@ int main() {
     nr.calcula_raiz(1.5); 
     nr_fl.calcula_raiz(1.5);
     nr_d.calcula_raiz(1.5);
-    
+
     print_vector(nr.get_iteracoes_de_x(), precisao);
     print_vector(nr_fl.get_iteracoes_de_x(), precisao);
     print_vector(nr_d.get_iteracoes_de_x(), precisao);
     teste_vizualizador();
+
+    teste_vizualizador(); 
     return 0;
 }
 
