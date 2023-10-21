3913f401fae5ceb24c3a8af6405480dd16ece7df
israel <israelnicolas940@gmail.com>
2023-10-21 17:15:45 -0300

    outputs padronizados com teste aleatório

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,41 @@
 #include "NewtonRaphson.hpp"
 #include "Pendulo.hpp"
 #include <iomanip>
+#include <cmath>
+#include <random>
+void teste_vizualizador() {
+std::vector<vizualizador::EntradasLinhaQuadroComparativo> entradas;
 
-void teste_vizualizador(){ 
-    Vizualizador v; 
-    std::vector<Polinomio> polinomios{Polinomio({1.0,-3.0,2.0}),Pendulo(10,1), Polinomio({77.0,29.0,38.0,21.0,57.0,66.99})} ; 
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_real_distribution<double> double_dist(1.0, 1000.0);
 
-    // Teste de vizualização 
-    NewtonRaphson nr(23,0.9);  
-    nr.original(polinomios[1]);
-    NewtonRaphson nr2(10,0.1);  
-    nr2.original(polinomios[1]);
-    v.print_quadro_coeficientes_fixos(polinomios[1],{&nr,&nr2},8); 
+for (int i = 0; i < 100; i++) {
+    int metodo = i % 3;  // Alternar entre os três métodos (0, 1, 2)
+    double a3 = double_dist(gen);
+    double a2 = double_dist(gen);
+    double lambda = double_dist(gen);
+    double epsilon = double_dist(gen);
 
+    entradas.push_back(vizualizador::EntradasLinhaQuadroComparativo{metodo, a3, a2, lambda,epsilon});
 }
-
+// std::vector<vizualizador::EntradasLinhaQuadroComparativo> entradas {
+//     {2, 4123.4124, 1245.2133002, 123.2, 999.3939},
+//     {0, 10.9, 22.0, 72.2},
+//     {1, 10.9, 22.0, 72.2},
+//     {0, 5.6, 11.0, 41.3},
+//     {1, 7.8, 16.5, 63.7},
+//     {2, 15.2, 31.4, 121.9},
+//     {0, 3.2, 7.0, 31.6},
+//     {1, 12.5, 28.8, 112.4},
+//     {2, 21.7, 42.3, 167.8},
+//     {0, 6.1, 13.7, 55.9}
+// };
+vizualizador::print_quadro_comparativo(entradas, 2);
+}
 int main() {
-    Polinomio p = Polinomio({1.0, -3.0, 2.0}); // x² - 3x + 2
-    //std::cout << p.get_grau() << std::endl;
-    teste_vizualizador();
+    
+    teste_vizualizador(); 
     return 0;
 }
 
--- src/metodos/NewtonRaphson.cpp
+++ src/metodos/NewtonRaphson.cpp
@@ -13,6 +13,19 @@ void NewtonRaphson::original(Polinomio polinomio){
    tabela.push_back(1.1);
    tabela.push_back(1.200);
    tabela.push_back(1.9998);
-   tabela.push_back(1.99999999);
+   tabela.push_back(1222222.99999999);
 
 }
+void NewtonRaphson::derivada_numerica(Polinomio polinomio){
+   num_passos = 222; 
+   for (int i = 0; i < num_passos; i ++) 
+      tabela.push_back(123.0);
+} 
+void NewtonRaphson::modificado(Polinomio polinomio){
+   num_passos = 5; 
+   tabela.push_back(13311.16); 
+   tabela.push_back(3222.1);
+   tabela.push_back(3.200);
+   tabela.push_back(3.9998);
+   tabela.push_back(3333222000.213);
+} 
