d615a5c10a36582dbc78c80ccbb45bb0bb70fcda
Bruno Sena Saunders <brunossaunders@pop-os.localdomain>
2023-10-22 00:09:58 -0300

    isolamento automatico

--- include/Polinomio.hpp
+++ include/Polinomio.hpp
@@ -20,7 +20,7 @@ public:
 
     bool tem_mais_raiz(double a, double b);
 
-    void encontra_intervalos(double a, double b);
+    void encontra_intervalos(double a, double b, int n_raizes);
     
     Polinomio divide(Polinomio p);
 
@@ -28,11 +28,17 @@ public:
 
     Polinomio multiplica(double valor);
 
+    Polinomio multiplica(Polinomio p);
+
     Polinomio soma(Polinomio p);
 
     Polinomio subtrai(Polinomio p);
 
-    std::pair<double, double> intervalo_max(std::vector<double> coef);
+    void print();
+
+    int numero_raizes(double a, double b);
+
+    std::pair<double, double> intervalo_max();
 
     std::vector<std::pair<double, double>> intervalos;
 }; 
--- src/main.cpp
+++ src/main.cpp
@@ -21,26 +21,6 @@ void print_vector(std::vector<double> v, int precisao) {
     std::cout << "]\n";
 }
 
-void teste_vizualizador() {
-    std::vector<vizualizador::EntradasLinhaQuadroComparativo> entradas;
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> double_dist(1.0, 1000.0);
-
-    for (int i = 0; i < 100; i++) {
-        int metodo = i % 3;  // Alternar entre os três métodos (0, 1, 2)
-        double a3 = double_dist(gen);
-        double a2 = double_dist(gen);
-        double lambda = double_dist(gen);
-        double epsilon = double_dist(gen);
-
-        entradas.push_back(vizualizador::EntradasLinhaQuadroComparativo{metodo, a3, a2, lambda,epsilon});
-    }
-    double intervalo[2] {1.23,34.9};
-    vizualizador::print_quadro_comparativo(entradas, 6,10,intervalo);
-}
-
 int main() {
     return 0;
 }
