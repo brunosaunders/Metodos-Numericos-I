a5cdc8d58dec8cfc92e9fb02d1f3dc72577001e8
Bruno Sena Saunders <brunossaunders@pop-os.localdomain>
2023-10-25 14:53:04 -0300

    bug fix no cálculo da derivada

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 
 
 void testa_passos() {
-    Pendulo p(1, -1);
+    Pendulo p(1, 1);
     NewtonRaphson* nr = new NewtonRaphson(100, 0.001, p);
     NewtonRaphson* nr_d = new NewtonRaphsonComDerivadaNumerica(100, 0.00001, p);
     Passos::exibir_passos(nr);
@@ -27,36 +27,33 @@ void testa_passos() {
 void teste_vizualizador() {
     std::vector<vizualizador::EntradasLinhaQuadroComparativo> entradas;
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> double_dist(1.0, 1000.0);
+    for (int i=1; i < 10; i++) {
+        for (int j=1; j < 10; j++) {
+            double a3 = j;
+            double a2 = i;
+            double epsilon = 0.0001;
 
-    for (int i = 0; i < 100; i++) {
-        int metodo = i % 3;  // Alternar entre os três métodos (0, 1, 2)
-        double a3 = double_dist(gen);
-        double a2 = double_dist(gen);
-        double lambda = double_dist(gen);
-        double epsilon = double_dist(gen);
-
-        entradas.push_back(vizualizador::EntradasLinhaQuadroComparativo{metodo, a3, a2, lambda,epsilon});
+            entradas.push_back(vizualizador::EntradasLinhaQuadroComparativo{1, a3, a2, 0.5, epsilon});
+        }
     }
+
     double intervalo[2] {1.23,34.9};
     vizualizador::print_quadro_comparativo(entradas, 6,10,intervalo);
 }
 
 int main() {
-    int precisao = 6;
-    std::vector<double> coef = {-2, 1, 1}; 
-    Polinomio p(coef); 
-    NewtonRaphson nr(100, 0.001, p); 
-    NewtonRaphsonFL nr_fl(100, 0.001, p, 0.5);
-    NewtonRaphsonComDerivadaNumerica nr_d(100, 0.001, p);
+    // int precisao = 6;
+    // std::vector<double> coef = {-2, 1, 1}; 
+    // Polinomio p(coef); 
+    // NewtonRaphson nr(100, 0.001, p); 
+    // NewtonRaphsonFL nr_fl(100, 0.001, p, 0.5);
+    // NewtonRaphsonComDerivadaNumerica nr_d(100, 0.001, p);
 
-    nr.calcula_raiz(1.5); 
-    nr_fl.calcula_raiz(1.5);
-    nr_d.calcula_raiz(1.5);
+    // nr.calcula_raiz(1.5); 
+    // nr_fl.calcula_raiz(1.5);
+    // nr_d.calcula_raiz(1.5);
 
-    teste_vizualizador();
+    // teste_vizualizador();
 
     //Testa passos
     testa_passos();
