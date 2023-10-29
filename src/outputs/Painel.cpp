28b020dab6b54ab04e73b76d51f010ee3921f038
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2023-10-29 01:24:29 -0300

    Update Painel.cpp

--- src/outputs/Painel.cpp
+++ src/outputs/Painel.cpp
@@ -1,62 +1,71 @@
 #include "Painel.hpp"
 #include "NewtonRaphson.hpp"
+#include "NewtonRaphsonFL.hpp"
+#include "NewtonRaphsonComDerivadaNumerica.hpp"
 #include "Polinomio.hpp"
+#include "Pendulo.hpp"
 #include "fmt/core.h"
 #include "fmt/format.h"
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 using namespace metodos_numericos1::include;
 
 Painel::Painel() {}
 
-void Painel::init() {
+void Painel::init(std::vector<NewtonRaphson*>& funcoes){
     int escolha_usuario;
     this->output(this->texto_menu_principal);
     std::cin >> escolha_usuario;
     switch (escolha_usuario) {
+        case 0:
+            {
+                break;
+            }
         case 1: 
             {
-                double a3, a2, precisao;
+                double a3, a2, precisao, lambda;
                 this->output(this->texto_cadastrar_funcao_metodo_analise);
-                std::cin >> a3 >> a3 >> precisao;
-                Polinomio polinomio = Polinomio({a3, a2, 3});
+                std::cin >> a3 >> a2 >> precisao;
+                Pendulo pendulo(a3, a2);
                 this->output(this->texto_escolher_newton_raphson);
                 std::cin >> escolha_usuario;
                 switch (escolha_usuario) {
                     case 1:
                         {
                             this->output(this->texto_newton_raphson_normal);
-                            //nr.met = "NR";
-                            //funcoes.push_back(nr);
+                            NewtonRaphson* nr = new NewtonRaphson(100, precisao, pendulo);
+                            funcoes.push_back(nr);
                             fmt::print("{0} NR(a3: {1}, a2: {2}, e: {3}) \n", this->texto_metodo_cadastrado, a3, a2, precisao);
+                            this->init(funcoes);
                             break;
                         }
                     case 2:
                         {
                             this->output(this->texto_newton_raphson_fl);
-                            //nr.met = "NR-Fl";
-                            //cout << "Informe o lambda:\n";
-                            //cin >> nr.lambda;
-                            //funcoes.push_back(nr);
-                            //this->output(this->texto_metodo_cadastrado, 1, 2);
+                            this->output(this->texto_cadastrar_lambda);
+                            std::cin >> lambda;
+                            NewtonRaphson* nrfl = new NewtonRaphsonFL(100, precisao, pendulo, lambda);
+                            funcoes.push_back(nrfl);
                             fmt::print("{0} NR-FL(a3: {1}, a2: {2}, e: {3}, lambda: {4}) \n", this->texto_metodo_cadastrado, a3, a2, precisao, lambda);
+                            this->init(funcoes);
                             break;
                         }
                     case 3: 
                         {
                             this->output(this->texto_newton_raphson_derivada_numerica);
-                            //nr.met = "NR-D";
-                            //funcoes.push_back(nr);
-                            //this->output(this->texto_metodo_cadastrado, "o", "p");
+                            NewtonRaphson* nrd = new NewtonRaphsonComDerivadaNumerica(100, precisao, pendulo);
+                            funcoes.push_back(nrd);
                             fmt::print("{0} NR-D(a3: {1}, a2: {2}, e: {3}) \n", this->texto_metodo_cadastrado, a3, a2, precisao);
+                            this->init(funcoes);
                             break;
                         }
                     default: 
                         {
                             this->output(this->texto_entrada_invalida);
-                            this->init();
+                            this->init(funcoes);
                             break;
                         }
                 }
@@ -65,96 +74,94 @@ void Painel::init() {
         case 2: 
             {
                 this->output(this->texto_remover_funcao_cadastrada);
-                //// Checar a possibilidade dessa ação acontecer
-                //if (funcoes.size() < 1)
-                //{
-                //    cout << "Ainda nao foram cadastradas funcoes. Portanto, nao e possivel remover nenhuma. ";
-                //    cout << "Tente inserir uma funcao primeiramente.\n\n";
-                //}
-                //else // Aqui pode acontecer
-                //{
-                //    int j;
-                //    cout << "Estas sao as funcoes cadastradas:\n\n";
-                //    for (int i = 0; i < funcoes.size(); i++) 
-                //    {
-                //        cout << i + 1 << " -> " << funcoes[i].met << "(" << funcoes[i].a3 << " " << funcoes[i].a2 << " " << funcoes[i].prec << " " << funcoes[i].lambda << ")\n";
-                //    }
-                //    cout << "\nInsira o indice de qual funcao deseja remover:\n";
-                //   cin >> j;
-                //    funcoes.erase(funcoes.begin() + (j - 1));
-                //    cout << "Funcao removida com sucesso.\n\n";
-                //}
-                this->init();
+                // Checar a possibilidade dessa ação acontecer
+                if (funcoes.size() < 1)
+                {
+                    this->output(this->texto_nenhuma_funcao_remover);
+                }
+                else // Aqui pode acontecer
+                {
+                    int j;
+                    this->output(this->texto_mostrar_funcoes_cadastradas);
+                    for (int i = 0; i < funcoes.size(); i++) 
+                    {
+                        std::cout << i+1 << " -> " << funcoes[i]->get_classe(2) << "\n";
+                    }
+                    this->output(this->texto_inserir_indice_remover);
+                   std::cin >> j;
+                    funcoes.erase(funcoes.begin() + (j - 1));
+                    this->output(this->texto_funcao_removida);
+                }
+                this->init(funcoes);
                 break;
             }
         case 3: 
             {
                 this->output(this->texto_resetar_funcoes_cadastradas);
-                //// Checar a possibilidade dessa ação ocorrer
-                //if (funcoes.size() < 1)
-                //{
-                //    cout << "Nao existem funcoes cadastradas. Portanto, nao ha o que remover.\n\n";
-                //}
-                //else
-                //{
-                //    funcoes.clear();
-                //    cout << "Pronto, todas as funcoes foram removidas.\n\n";
-                //}
-                this->init();
+                // Checar a possibilidade dessa ação ocorrer
+                if (funcoes.size() < 1)
+                {
+                    this->output(this->texto_sem_funcoes_resetar);
+                }
+                else
+                {
+                    funcoes.clear();
+                    this->output(this->texto_funcoes_resetadas);
+                }
+                this->init(funcoes);
                 break;
             }
         case 4: 
             {
                 this->output(this->texto_analise_passo_passo_funcao);
-                //if (funcoes.size() < 1)
-                //{
-                //    cout << "Ainda nao foram cadastradas funcoes. Portanto, nao e possivel analisar nenhuma. ";
-                //    cout << "Tente inserir uma funcao primeiramente.\n\n";
-                //}
-                //else
-                //{
-                //    int j;
-                //    cout << "Estas sao as funcoes cadastradas:\n\n";
-                //    for (int i = 0; i < funcoes.size(); i++)
-                //    {
-                //        cout << i + 1 << " -> " << funcoes[i].met << "(" << funcoes[i].a3 << " " << funcoes[i].a2 << " " << funcoes[i].prec << " " << funcoes[i].lambda << ")\n";
-                //    }
-                //    cout << "\nInsira o indice de qual funcao deseja analisar:\n";
-                //    cin >> j;
-                //    if (funcoes[j - 1].met == "NR")
-                //    {
-                //        //chamar metodo NR
-                //    }
-                //    else if (funcoes[j - 1].met == "NR-FL")
-                //    {
-                //        //chamar metodo NR-FL
-                //    }
-                //    else if (funcoes[j - 1].met == "NR-D")
-                //    {
-                //        //chamar metodo NR-D
-                //    }
-                //}
-                this->init();
+                if (funcoes.size() < 1)
+                {
+                   this->output(this->texto_sem_funcoes_analisar);
+                }
+                else
+                {
+                    int j;
+                    this->output(this->texto_mostrar_funcoes_cadastradas);
+                    for (int i = 0; i < funcoes.size(); i++)
+                    {
+                       std::cout << i+1 << " -> " << funcoes[i]->get_classe(2) << "\n";
+                    }
+                    this->output(this->texto_inserir_indice_analisar);
+                    std::cin >> j;
+                    if (funcoes[j - 1]->get_nome_abreviado() == "NR")
+                    {
+                        std::cout << "NR\n"; //inserir aqui o metodo de analise
+                    }
+                    else if (funcoes[j - 1]->get_nome_abreviado() == "NR-FL")
+                    {
+                        std::cout << "NR-FL\n"; //inserir aqui o metodo de analise
+                    }
+                    else if (funcoes[j - 1]->get_nome_abreviado() == "NR-D")
+                    {
+                        std::cout << "NR-D\n"; //inserir aqui o metodo de analise
+                    }
+                }
+                this->init(funcoes);
                 break;
             }
         case 5: 
             {
                 this->output(this->texto_mostrar_quadro_comparativo_func_cadastradas);
                 // Chamar método que mostra quadro comparativo
-                this->init();
+                this->init(funcoes);
                 break;
             }
         case 6: 
             {
                 this->output(this->texto_mostrar_isolamento_funcao);
                 // Chamar método que mostra o isolamento da função
-                this->init();
+                this->init(funcoes);
                 break;
             }
         default: 
             {
                 this->output(this->texto_entrada_invalida);
-                this->init();
+                this->init(funcoes);
                 break;
             }
     }
