f1a66287aca80adb9fadb577739e73d7ffdb355c
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2023-10-29 01:07:50 -0300

    Create Tabela.hpp

---
+++ include/Tabela.hpp
@@ -0,0 +1,20 @@
+#ifndef TABELA_HPP
+#define TABELA_HPP
+#include <string> 
+
+namespace metodos_numericos1::outputs
+{
+
+    class Tabela { 
+    public:
+        static void formata_cabecalho(std::string palavra, int tamanho_coluna);
+        static void formata_numero_cientifico(double numero, int precisao, int largura_total);
+        static void formata_numero_normal(double numero, int largura_total, int padding);
+        static void exibir_tampa_tabela(int tamanho);
+        static void formata_palavra(std::string palavra, int largura_total);
+        static void formata_numero(double numero, int largura_total);
+        static void formata_numero(int numero, int largura_total);
+    }; 
+} 
+
+#endif
