4a298260114f55a2ca5f55d9554f76b70cc66652
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2023-10-29 01:01:26 -0300

    Update Polinomio.hpp

--- include/Polinomio.hpp
+++ include/Polinomio.hpp
@@ -1,20 +1,58 @@
-#ifndef POLINOMIO_HPP
-#define POLINOMIO_HPP
+#ifndef METODOS_NUMERICOS1_INCLUDE_POLINOMIO_HPP_
+#define METODOS_NUMERICOS1_INCLUDE_POLINOMIO_HPP_
+
 #include <vector> 
 
-class Polinomio { 
-public:
-    Polinomio(); 
-    Polinomio(std::vector<double> coeficientes); 
+namespace metodos_numericos1::include {
+    class Polinomio { 
+        public:
+            Polinomio(); 
+            Polinomio(std::vector<double> coeficientes); 
 
-    int get_grau() const;
-    double get_valor_funcao(double x) const; // Método que retorna P(x) 
-    std::vector<double> coeficientes; 
+            int get_grau() const;
+            double get_valor_funcao(double x) const; // Método que retorna P(x) 
+            std::vector<double> coeficientes; 
 
-    Polinomio get_funcao_derivada() const;
+            Polinomio get_funcao_derivada() const;
 
-    /*acessa o coeficiente a_i*/
-    double operator[] (int i) const;
-    double p(double x) const;
-}; 
-#endif
+            /*acessa o coeficiente a_i*/
+            double operator[] (int i) const;
+
+            double p(double x) const;
+
+            /*
+             * Dado um polinômio retorna intervalos contendo raizes desse polinômi desse polinômioo 
+             */
+            void encontra_intervalos(double a, double b, int n_raizes);
+
+            Polinomio divide(Polinomio p);
+
+            Polinomio divide(double valor);
+
+            Polinomio multiplica(double valor);
+
+            Polinomio multiplica(Polinomio p);
+
+            Polinomio soma(Polinomio p);
+
+            Polinomio subtrai(Polinomio p);
+
+            void print();
+
+            /*
+             * Dado um intervalo é retornado a quantidade de raízes dentro daquele intervalo 
+             * Esse método faz uso do teorema de Sturm para fazer o cálculo da quantidade de raizes existentes no intervalo dado
+             */
+            int numero_raizes_reais(double a, double b);
+
+            /*
+             * Retorna um intervalo onde as raízes do polinômio se encontram.
+             * Esse método faz o uso do Cauchy's bounds.
+             */
+            std::pair<double, double> intervalo_max();
+
+            std::vector<std::pair<double, double>> intervalos;
+    }; 
+} // namespace metodos_numericos1::include
+
+#endif /* METODOS_NUMERICOS1_INCLUDE_POLINOMIO_HPP_ */
