c64097e3b28ba11f7be4bfc7511a59848edf3a91
Bruno Sena Saunders <brunossaunders@pop-os.localdomain>
2023-11-14 12:15:49 -0300

    trabalho legado como diretório oculto

--- trabalho-legado/include/Utils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef METODOS_NUMERICOS1_INCLUDE_UTILS_HPP
-#define METODOS_NUMERICOS1_INCLUDE_UTILS_HPP
-
-#include <vector> 
-
-namespace metodos_numericos1::outputs { 
-    void print_vector(std::vector<double> v);
-}; 
-#endif
