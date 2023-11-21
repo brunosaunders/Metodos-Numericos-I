b1329504240c0ebcaafd959ccff2d50dff1f398d
Bruno Sena Saunders <brunossaunders@pop-os.localdomain>
2023-11-21 16:10:21 -0300

    getters vetor_d e vetor_amplitudes

--- src/metodos/Gauss.cpp
+++ src/metodos/Gauss.cpp
@@ -1,12 +1,32 @@
 #include "Gauss.hpp"
+#include <stdexcept>
 
 using namespace std;
 using namespace metodos_numericos1::metodos;
 
 void Gauss::resolve_sistema_linear() {
+    // TODO
     return;
 }
 
+double Gauss::get_a() {
+    return this->a;
+}
+
+vector<double> Gauss::get_vetor_d_deslocamentos() {
+    if (this->vetor_d_deslocamentos.size() == 0) {
+        throw runtime_error("Chame resolve_sistema_linear() antes.");
+    }
+    return this->vetor_d_deslocamentos;
+}
+
+vector<double> Gauss::get_vetor_amplitudes() {
+    vector<double> vetor_amplitudes = this->get_vetor_d_deslocamentos();
+    for (auto& item : vetor_amplitudes) {
+        item *= this->a;
+    }
+    return vetor_amplitudes;
+}
 
 vector<double> Gauss::get_vetor_v() {
     return this->vetor_v;
